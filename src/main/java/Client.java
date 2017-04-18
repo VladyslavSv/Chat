21b3c1fe3f6bd2c90b0d915f964dbe30456bfaec
Vladislav Shulyanskiy <everysundayhere@gmail.com>
2017-04-18 13:13:49 +0300

    Good context menu delete

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -27,8 +27,8 @@ public class Client implements Initializable{
     private ClientExecutor clientExecutor;
     private ArrayList<String> allOnline = new ArrayList<>();
     private Map<String, Tab> openTabs = new HashMap<>();
+    private Map<Tab,ContextMenu> hope = new HashMap<>();
     private String myName;
-    private ContextMenu contextMenu;
 
     public Client() {
         myName = Login.userName;
@@ -40,15 +40,6 @@ public class Client implements Initializable{
     }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        contextMenu = new ContextMenu();
-        MenuItem item = new MenuItem("Close");//В контекстом меню один пункт
-        item.setOnAction(event -> {//при нажатии на close
-            openTabs.remove(tabPane.getSelectionModel().getSelectedItem().getText());//из мапы удаляется запись
-            if(tabPane.getSelectionModel().getSelectedItem() != tabMain) {//просто чтобы не закрыть main
-                tabPane.getTabs().remove(tabPane.getSelectionModel().getSelectedItem());//удаляется не та вкладка по которой кликнули, а активная(ПРОБЛЕМА)
-            }
-        });
-        contextMenu.getItems().add(item);//В контекст добавляю пункт
         tArea.setEditable(false);
         try {
             HelperForClient.getConnection().send(new Message(MessageType.REQUEST_FOR_ONLINE,null));
@@ -56,6 +47,71 @@ public class Client implements Initializable{
             e.printStackTrace();
         }
     }
+    private class ClientExecutor extends Thread {
+        public void run() {
+            while (true) {
+                try {
+                    message = connection.receive();
+                    switch (message.getMessageType()) {
+                        case SEND_PERSONAL_MESSAGE://Получаем личное сообщение от сервера
+                            sendPersonalMessage();
+                            break;
+                        case BROAD_CAST:
+                            tArea.appendText(message.getData() + "\n");
+                            break;
+                        case ADD_TO_ONLINE:
+                            addToOnline();
+                            break;
+                        case REMOVE_FROM_ONLINE:
+                            removeFromOnline();
+                            break;
+                    }
+                } catch (ClassNotFoundException | IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void sendPersonalMessage(){
+        if(openTabs.containsKey(message.getSender())) {//Если вкладка с личкой открыта
+            selfMessage(message);//переписываем сообщение себе в чат
+        }else{//вкладка с личкой закрыта
+            Platform.runLater(() -> {
+                openTab(message.getSender());//Открываем вкладку отправителя у себя
+                selfMessage(message);////переписываем сообщение себе в чат
+            });
+        }
+    }
+    private void addToOnline(){
+        allOnline.add(message.getData());
+        Platform.runLater(() -> {
+            vBox.getChildren().clear();
+            for(String s : allOnline) {
+                Label l = new Label(s);
+                l.setOnMouseClicked(event -> {
+                    if (event.getButton().equals(MouseButton.PRIMARY)) {
+                        if (event.getClickCount() == 2) {
+                            int sz = l.getText().length();
+                            openTab(l.getText().substring(0,sz-1));//Без переноса на новую строку таб нужен
+                        }
+                    }
+                });
+                l.setId(s);
+                vBox.getChildren().add(l);
+            }
+        });
+    }
+    private void removeFromOnline(){
+        Platform.runLater(() -> {
+            vBox.getChildren().remove(
+                    vBox.getChildren().get(
+                            allOnline.indexOf(
+                                    message.getData())));
+            allOnline.remove(message.getData());
+        });
+    }
+
     public void onButtonClick() {
         try {
             if(tField.getText().length()>0) {
@@ -103,6 +159,10 @@ public class Client implements Initializable{
             tabPane.getSelectionModel().select(openTabs.get(tabName));//переходим в другую вкладку
         }else {
             Tab myNewTab = new Tab(tabName);//создаю вкладку
+
+            ContextMenu contextMenu = createContextMenu();
+            hope.put(myNewTab,contextMenu);
+
             myNewTab.setContextMenu(contextMenu);//устанавливаю контекстное меню
             TextArea t = new TextArea();
             t.setPrefSize(462.0,272.0);
@@ -114,9 +174,25 @@ public class Client implements Initializable{
             myNewTab.setOnClosed(e -> openTabs.remove(tabName));//при закрытии вкладки удаляется запись из открытых вкладок
         }
     }
+    private ContextMenu createContextMenu(){
+        ContextMenu contextMenu = new ContextMenu();//Под каждый таб создаю контекст меню
+        MenuItem item = new MenuItem("Close");//В контекстом меню один пункт
+        item.setOnAction(event -> {//при нажатии на close
+            for (Tab tab : hope.keySet()) {
+                if (hope.get(tab) == item.getParentPopup()) {//getParentPopUp() - контекст меню нажатого таба
+                    if(tabPane.getSelectionModel().getSelectedItem() == tab)//Если удаляешь активную вкладку
+                        tabPane.getSelectionModel().selectFirst();//переключаешься на первую
 
-
-
+                    tabPane.getTabs().remove(tab);
+                    openTabs.remove(tab.getText());
+                    hope.remove(tab);
+                    break;
+                }
+            }
+        });
+        contextMenu.getItems().add(item);//В контекст добавляю пункт
+        return contextMenu;
+    }
     private void selfMessage(Tab tab){
         AnchorPane a = (AnchorPane) tab.getContent();//Во вкладке есть anchorPane
         TextArea ta = (TextArea) a.getChildren().get(0);//В anchorPane есть TextArea
@@ -128,60 +204,4 @@ public class Client implements Initializable{
         TextArea tAre = (TextArea) a.getChildren().get(0);//По идее должны получить TextArea local к Табу
         tAre.appendText(m.getData());
     }
-
-    private class ClientExecutor extends Thread {
-        public void run() {
-            while (true) {
-                try {
-                    message = connection.receive();
-                    switch (message.getMessageType()) {
-                        case SEND_PERSONAL_MESSAGE://Получаем личное сообщение от сервера
-                            if(openTabs.containsKey(message.getSender())) {//Если вкладка с личкой открыта
-                                selfMessage(message);//переписываем сообщение себе в чат
-                            }else{//вкладка с личкой закрыта
-                                Platform.runLater(() -> {
-                                    openTab(message.getSender());//Открываем вкладку отправителя у себя
-                                    selfMessage(message);////переписываем сообщение себе в чат
-                                });
-                            }
-                            break;
-                        case BROAD_CAST:
-                            tArea.appendText(message.getData() + "\n");
-                            break;
-                        case ADD_TO_ONLINE:
-                            allOnline.add(message.getData());
-                            Platform.runLater(() -> {
-                                vBox.getChildren().clear();
-                                for(String s : allOnline) {
-                                    Label l = new Label(s);
-                                    l.setOnMouseClicked(event -> {
-                                        if (event.getButton().equals(MouseButton.PRIMARY)) {
-                                            if (event.getClickCount() == 2) {
-                                                int sz = l.getText().length();
-                                                openTab(l.getText().substring(0,sz-1));//Без переноса на новую строку таб нужен
-                                            }
-                                        }
-                                    });
-                                    l.setId(s);
-                                    vBox.getChildren().add(l);
-                                }
-                            });
-                            break;
-                        case REMOVE_FROM_ONLINE:
-                            Platform.runLater(() -> {
-                                vBox.getChildren().remove(
-                                        vBox.getChildren().get(
-                                                allOnline.indexOf(
-                                                        message.getData())));
-                                allOnline.remove(message.getData());
-                            });
-                            break;
-                    }
-                } catch (ClassNotFoundException | IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
 }
